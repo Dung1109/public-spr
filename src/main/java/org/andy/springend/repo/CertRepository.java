5e1721683e2584a467c7b5f422792158919ea130
Dung1109 <124704925+Dung1109@users.noreply.github.com>
2024-11-23 12:56:09 +0700

    add paging

--- src/main/java/org/andy/springend/repo/CertRepository.java
+++ src/main/java/org/andy/springend/repo/CertRepository.java
@@ -4,7 +4,10 @@ import org.andy.springend.entities.Cert;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CertRepository extends JpaRepository<Cert, String> {
+
+    @Query("SELECT c FROM Cert c")
     Page<Cert> findAllWithPagination(Pageable pageable);
 }
